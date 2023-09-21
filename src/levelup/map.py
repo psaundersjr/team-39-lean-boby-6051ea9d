2ebfb2e3cecc009e5a5e324763d1bf17e1979df3
Jay <jbirdjj@aol.com>
2023-09-21 15:20:50 +0000

    Initialize map class

---
+++ src/levelup/map.py
@@ -0,0 +1,17 @@
+class Map:
+    numPositions = 100
+
+    def getPositions ():
+        pass
+
+    def calculatePosition(Position startingPosition, DIRECTION direction) ():
+        pass
+    
+    def isPositionValid(Point positionCoordinates) ():
+        pass
+
+    def getTotalPositions ():
+        pass
+
+
+
