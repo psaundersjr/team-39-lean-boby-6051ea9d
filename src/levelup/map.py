c808a18d26fd8d39cc62f84c785537370c8fc1f7
eTech <finleywilliam@gmail.com>
2023-09-21 20:05:22 +0000

    adding references to Position and Direction

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,13 +1,15 @@
-import levelup
+from levelup.position import Position
+from levelup.controller import Direction
 class Map:
     numPositions = 100
 
     def getPositions ():
         pass
 
-   # def calculateposition(startingPosition:Position, direction:Direction):
-   #     pass
-    
+    def calculateposition(startingPosition: Position, direction: Direction):
+       pass
+
+        
     def isPositionValid(positionCoordinateX,positionCoordinateY):
         pass
 
